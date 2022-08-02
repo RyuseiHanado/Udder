a83a0474f6c04ef11083c103be945ff5cc204a0f
RyuseiHanado <riyusei_hanado@mail.toyota.co.jp>
2022-08-02 21:04:58 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by 花堂　瑠聖 on 2022/08/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
